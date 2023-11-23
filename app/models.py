00261aa8f4c157a173f88b5ca11e545c6152c671
andaeys <ekayudha.sulistianda@quipper.com>
2023-11-23 16:16:27 +0700

    add product and shopping chart models

--- app/models.py
+++ app/models.py
@@ -8,3 +8,21 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.username}>'
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    stock_quantity = db.Column(db.Integer, nullable=False)
+
+
+class ShoppingCart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    user = db.relationship('User', backref=db.backref('shopping_cart', lazy=True))
+
+
