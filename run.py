98e7d40aa7be6341069379b38df8eacd4af08cc1
andaeys <ekayudha.sulistianda@quipper.com>
2023-11-23 16:22:30 +0700

    add auto migrate db on run

--- run.py
+++ run.py
@@ -1,12 +1,18 @@
 import time
 
 from app import app, db
+from flask_migrate import Migrate
+
+# Initialize Flask-Migrate
+migrate = Migrate(app, db)
 
 # Initialize database
 with app.app_context():
     try:
         time.sleep(1)
         db.create_all()
+        migrate.init_app(app)
+        migrate.upgrade()
         print("db Tables created successfully")
     except Exception as e:
         print(f"db Error creating tables: {e}")
