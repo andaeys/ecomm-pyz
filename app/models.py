5e2bc4e3519a1a0c52aeec48ba3be799776caa43
andaeys <ekayudha.sulistianda@quipper.com>
2023-11-23 16:35:13 +0700

    update user model with account

--- app/models.py
+++ app/models.py
@@ -1,10 +1,26 @@
 from app import db
+from flask_bcrypt import Bcrypt
+from flask_login import UserMixin
+
+bcrypt = Bcrypt()
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+    # Add a relationship to ShoppingCart
+    shopping_cart = db.relationship('ShoppingCart', backref='user', lazy=True)
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password, password)
 
     def __repr__(self):
         return f'<User {self.username}>'
